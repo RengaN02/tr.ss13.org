ca66824605c745949364d7f27a6c3b1fc2b539ea
Rengan <takibrine07@gmail.com>
2026-01-20 17:44:27 +0300

    a

--- app/proxy/auth.ts
+++ app/proxy/auth.ts
@@ -10,22 +10,22 @@ export default {
 	},
 	async action(request) {
 		const token = await getToken({
-      req: request,
-      secret: process.env.NEXTAUTH_SECRET
-    });
+			req: request,
+			secret: process.env.NEXTAUTH_SECRET
+		});
 
-    if (!token) {
-      const url = request.nextUrl.clone();
-      url.pathname = '/login';
-      url.searchParams.set('callbackUrl', request.url);
-      return NextResponse.redirect(url);
-    }
+		if (!token) {
+			const url = request.nextUrl.clone();
+			url.pathname = '/login';
+			url.searchParams.set('callbackUrl', request.url);
+			return NextResponse.redirect(url);
+		}
 
 		if(!token.ckey && !request.nextUrl.pathname.startsWith('/verify')) {
-      const url = request.nextUrl.clone();
-      url.pathname = '/verify';
-      url.searchParams.set('callbackUrl', request.url);
-      return NextResponse.redirect(url);
+			const url = request.nextUrl.clone();
+			url.pathname = '/verify';
+			url.searchParams.set('callbackUrl', request.url);
+			return NextResponse.redirect(url);
 		}
 
 	},
